cd28e658269e33444a37d53aebaafaebf75013c7
Vijay-Patidar-2226 <vijayharniya6267@gmail.com>
2024-07-04 11:20:17 +0530

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+int main(){
+int a,b,c;
+printf(" enter a and b \n");
+
+scanf("%d %d",&a,&b);
+c=a+b;
+printf("%d",c);
+
+
+
+return 0;
+}
+/*#include<stdio.h>
+int add(int,int);
+int main(){
+int a,b,c;
+printf("enter a and  b : ");
+scanf("%d %d",&a ,&b);
+c=add(a,b);
+printf("%d",c);
+return 0;
+}
+
+int add(int a,int b){
+ int sum;
+sum=a+b;
+return sum;
+}*/
